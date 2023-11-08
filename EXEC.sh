dedda1abbd64d0154501963e7bf59829e9b4761b
Miles Kent <64712073+AlexKCA@users.noreply.github.com>
2023-11-08 05:06:35 +0000

    Update EXEC.sh

--- EXEC.sh
+++ EXEC.sh
@@ -3,6 +3,10 @@ sudo usermod -aG sudo alexkca
 sudo apt update && sudo apt upgrade
 
 # TODO: Tap to Click
+# https://unix.stackexchange.com/questions/337008/activate-tap-to-click-on-touchpad
+# https://askubuntu.com/questions/403113/how-do-you-enable-tap-to-click-via-command-line
+
+
 # Flat mouse
 # Dark theme
 # Remove hot corner
@@ -11,10 +15,11 @@ sudo apt update && sudo apt upgrade
 # build-essential
 # neovim wl-clipboard
 
+
 # github ssh
 # gh
 
 # firefox addons
 # bismuth
 
-sudo reboot
+systemctl reboot
