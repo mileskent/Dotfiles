05139641722490a9a7d13c52c857b52a6f2b16a9
AlexKCA <alexkca@endor.endor>
2023-11-15 12:01:31 -0800

    changed neovim config

---
+++ nvim/init.lua
@@ -0,0 +1,9 @@
+vim.cmd('syntax on')
+vim.cmd('colorscheme default')
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.mouse = 'a'
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.cursorline = true
+vim.cmd('hi CursorLine cterm=bold ctermbg=black')
--- nvim/init.vim
+++
@@ -1,9 +0,0 @@
-syntax on
-colorscheme default
-set tabstop=4
-set shiftwidth=4
-set mouse=a
-set number
-set relativenumber
-set cursorline
-hi CursorLine cterm=bold ctermbg=black
