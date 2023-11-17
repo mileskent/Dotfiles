3c934f4977d7d6500619c0ea0831c59a8aeedb93
AlexKCA <mileskent13@gmail.com>
2023-11-16 16:58:07 -0800

    Update dwm config

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -9,14 +9,16 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=14" };
 static const char dmenufont[]       = "monospace:size=14";
 static const char col_gray1[]       = "#222222";
+static const char col_darkpurple[]  = "#07051f";
+static const char col_black[	]   = "#000000";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray3, col_darkpurple, col_gray2 },
+	[SchemeSel]  = { col_gray3, col_black,  col_gray2  },
 };
 
 /* tagging */
