276f2640fdaeb013633818d7e4b18eb9b15733e3
AlexKCA <mileskent13@gmail.com>
2023-11-20 22:24:52 -0800

    update dwm

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -6,8 +6,8 @@ static const unsigned int gappx     = 10;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=16" };
-static const char dmenufont[]       = "monospace:size=16";
+static const char *fonts[]          = { "Agave Nerd Font:size=16" };
+static const char dmenufont[]       = "Agave Nerd Font:size=16";
 static const char col_gray1[]       = "#222222";
 static const char col_darkpurple[]  = "#07051f";
 static const char col_black[	]   = "#000000";
