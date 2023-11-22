96beb3ee60d150a7080e62fe8351cbb9d7b8c4e0
AlexKCA <mileskent13@gmail.com>
2023-11-21 23:08:48 -0800

    added stuff

--- suckless/udbar.sh
+++ suckless/udbar.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 
+getwifi() {
+    ssid=$(iwctl station wlan0 show | grep "Connected network" | awk '{print $3}')
+    if [ -n "$ssid" ]; then
+        echo "󰖩  $ssid"
+    else
+        echo "󰖪 "
+    fi
+}
+
 counter=0
 while :; do
-    if [ $((counter % 60)) -eq 0 ]; then
-        echo Updating weather
-        weather=$(curl wttr.in/?format="%C+%t")
+    # quarterhourly update
+    if [ $((counter % 900)) -eq 0 ]; then
+        weather=$(curl wttr.in/?format="%t")
+        dateinfo=$(date +"%a, %b %d")
     fi
-    dateinfo=$(date +"%a, %b %d %H:%M")
-    xsetroot -name "| $weather | $dateinfo |"
-    sleep 1m
-    $((counter++))
+
+    # minutely update
+    # if [ $((counter % 60)) -eq 0 ]; then
+    # fi
+
+    # secondly update
+    time=$(date +"%H:%M:%S")
+    volume=$(pamixer --get-volume)
+
+    # set bar
+    xsetroot -name "|  $weather | $(getwifi) | 󱄠 $volume% |   $dateinfo |  $time |"
+    sleep 1s
+    let 'counter++'
 done
 
