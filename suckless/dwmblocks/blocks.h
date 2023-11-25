22cdf147141bd54300f50c756af61b43bde569d4
AlexKCA <mileskent13@gmail.com>
2023-11-25 18:42:54 -0500

    update blocks.h

--- suckless/dwmblocks/blocks.h
+++ suckless/dwmblocks/blocks.h
@@ -1,13 +1,11 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
     {"dwm", "", 0, 0},
-    {"", "./commands/gettemp.sh", 15*60, 0}, // temp in F and thermon icon
-    {"", "./commands/getwifi.sh", 5, 0}, // Ssid and wifi icon
-    {"", "./commands/getvolume.sh", 1, 0}, // volume
-    {" ", "date +'%a, %b %d'", 1, 0}, // date
-    {" ", "date +'%H:%M:%S'", 1, 0}, // time
-    {"", "", 0, 0}
+    {"", "~/.suckless/dwmblocks/commands/gettemp.sh", 15*60, 0}, // temp in F and thermon icon
+    {"", "~/.suckless/dwmblocks/commands/getwifi.sh", 5, 0}, // Ssid and wifi icon
+    {"", "~/.suckless/dwmblocks/commands/getvolume.sh", 1, 0}, // volume
+    {"  ", "date +'%a, %b %d'", 1, 0}, // date
+    {" ", "date +'%H:%M:%S'", 1, 0} // time
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
