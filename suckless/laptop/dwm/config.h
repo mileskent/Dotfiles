33481fcace9601b048c0330b4ffe7c852028e417
AlexKCA <mileskent13@gmail.com>
2024-01-16 09:07:24 -0800

    updated laptop suckless
    added larger font

--- suckless/laptop/dwm/config.h
+++ suckless/laptop/dwm/config.h
@@ -6,8 +6,8 @@ static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Agave Nerd Font:size=22" };
-static const char dmenufont[]       = "Agave Nerd Font:size=22";
+static const char *fonts[]          = { "Agave Nerd Font:size=24" };
+static const char dmenufont[]       = "Agave Nerd Font:size=24";
 
 
 static const char *colors[][3]      = {
