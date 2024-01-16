9131898e3a6791a853a7214cad6f791c2cfd5975
AlexKCA <mileskent13@gmail.com>
2024-01-16 09:03:01 -0800

    updated suckless

---
+++ suckless/laptop/laptop-suckless/dwmblocks/commands/getvolume.sh
@@ -0,0 +1,13 @@
+getvolume() {
+    volumelevel=$(pamixer --get-volume)
+    if [ "$volumelevel" -ge 50 ]; then
+        icon="🔊"
+    elif [ "$volumelevel" -gt 0 ]; then
+        icon="🔉"
+    else
+        icon="🔈"
+    fi
+    echo "$icon$volumelevel%"
+}
+
+getvolume
