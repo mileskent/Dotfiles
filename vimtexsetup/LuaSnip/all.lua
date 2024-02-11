f914dbfdecaf08407bb16117cae8a23f844c9e61
AlexKCA <mileskent13@gmail.com>
2024-02-10 21:03:56 -0800

    added vimtex

---
+++ vimtexsetup/LuaSnip/all.lua
@@ -0,0 +1,10 @@
+-- Place this in ${HOME}/.config/nvim/LuaSnip/all.lua
+
+return {
+  -- A snippet that expands the trigger "hi" into the string "Hello, world!".
+  require("luasnip").snippet(
+    { trig = "hi" },
+    { t("Hello, world!") }
+  ),
+}
+
---
+++ vimtexsetup/init.lua
@@ -0,0 +1,39 @@
+-- PACKER
+require('plugins')
+
+-- GENERAL
+vim.cmd[[
+syntax enable
+]]
+
+-- VIMTEX
+vim.cmd[[
+filetype plugin indent on
+let g:vimtex_view_method = 'zathura'
+let maplocalleader = "\\"
+]]
+
+-- LUASNIPS
+vim.cmd[[
+" Use Tab to expand and jump through snippets
+imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
+smap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
+
+" Use Shift-Tab to jump backwards through snippets
+imap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+]]
+
+-- Load snippets from ~/.config/nvim/LuaSnip/
+require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/LuaSnip/"})
+
+-- Somewhere in your Neovim startup, e.g. init.lua
+require("luasnip").config.set_config({ -- Setting LuaSnip config
+
+  -- Enable autotriggered snippets
+  enable_autosnippets = true,
+
+  -- Use Tab (or some other key if you prefer) to trigger visual selection
+  store_selection_keys = "<Tab>",
+})
+
